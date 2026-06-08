5192161ea62c2410b7bfbf1350f4d66a44b13a36
John Bolliger <johnbolliger@gmail.com>
2026-06-07 22:37:35 -0700

    more

--- bench/real_server.sh
+++ bench/real_server.sh
@@ -5,4 +5,6 @@ export PB_CLIENT_TYPE="protobuf/nats/client"
 
 export PB_NATS_SERVER_SLOW_START_DELAY=1
 
-bundle exec rpc_server start --threads=20 ./examples/warehouse/app.rb
+export PB_NATS_SERVER_MAX_QUEUE_SIZE=6
+
+bundle exec rpc_server start --threads=2 ./examples/warehouse/app.rb
